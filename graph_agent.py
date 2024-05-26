431724c789a3d6022929e290e3ef9be4828c19c1
0msys <>
2024-05-26 17:52:15 +0000

    vision toolを実装

--- graph_agent.py
+++ graph_agent.py
@@ -1,5 +1,8 @@
 import json
 import operator
+import base64
+import os
+import requests
 
 from langchain.agents import tool
 from langchain_community.tools import DuckDuckGoSearchResults
@@ -12,9 +15,9 @@ from typing import TypedDict, Annotated, Sequence
 
 
 # 使用するモデルを選択。
-# 予算が許す場合はgpt-4-turboを推奨
+# 予算が許す場合はgpt-4oを推奨
 MODEL = "gpt-3.5-turbo-0125"
-# MODEL = "gpt-4-turbo"
+# MODEL = "gpt-4o"
 
 
 # Agentの使用できるToolを定義
@@ -25,10 +28,51 @@ async def ddg_search(query: str) -> str:
     search = DuckDuckGoSearchResults()
     return search.invoke(query)
 
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+@tool
+async def vision(prompt: str, image_paths: Sequence[str]) -> str:
+    """Pass multiple images to the multimodal AI to get results"""
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {os.getenv('OPENAI_API_KEY')}"
+    }
+
+    payload = {
+        "model": "gpt-4o",
+        "messages": [
+            {
+            "role": "user",
+            "content": [
+                {
+                "type": "text",
+                "text": prompt
+                },
+            ]
+            }
+        ],
+        "max_tokens": 300
+    }
+
+    for image_path in image_paths:
+        base64_image = encode_image(image_path)
+        payload["messages"][0]["content"].append({
+            "type": "image_url",
+            "image_url": {
+                "url": f"data:image/jpeg;base64,{base64_image}",
+                "detail": "low"
+            }
+        })
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    
+    return response.json()["choices"][0]["message"]["content"]
 
 # toolを配列にまとめて、ToolExecutorに渡す
 # toolを追加した場合は、忘れずにここに追加してください
-tools = [ddg_search]
+tools = [ddg_search, vision]
 tool_executor = ToolExecutor(tools)
 
 
